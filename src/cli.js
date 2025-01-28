21e906a8f92c2e0b5c0ba023c900e8173effb3c9
gilgunteer <maksimmikulskij@Noutbuk-Maksim.local>
2025-01-28 16:12:52 +0300

    danik lox

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+const  readlineSync  = require('readline-sync'); 
+ 
+const userName  = readlineSync.question(' May I have your name?'); 
+// console.log('Hello' +  userName  + '!'); ​​  //
+console.log('Hello ' + userName + '!');
+
